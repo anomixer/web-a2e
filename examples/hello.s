302a16c24ebe3a5885ec9702d1c34fae34aead84
michaeldaley <michael_daley@icloud.com>
2026-02-05 10:57:14 +0000

    Add character delay to hello.s example

--- examples/hello.s
+++ examples/hello.s
@@ -21,6 +21,9 @@ START    LDX  #$00        ;Initialize index to 0
 LOOP     LDA  MESSAGE,X   ;Load character at MESSAGE+X
          BEQ  DONE        ;If zero (end of string), we're done
          JSR  COUT        ;Print the character
+         PHX              ;Save X on stack (65C02)
+         JSR  DELAY       ;Wait a bit
+         PLX              ;Restore X from stack (65C02)
          INX              ;Increment index
          BNE  LOOP        ;Continue if X hasn't wrapped
 
@@ -28,6 +31,20 @@ LOOP     LDA  MESSAGE,X   ;Load character at MESSAGE+X
 * End of program
 **********************************
 DONE     RTS              ;Return to caller
+
+**********************************
+* Delay routine - nested loop
+* Adjust WAIT value for longer/shorter delay
+**********************************
+WAIT     EQU  $60         ;Delay multiplier (~100ms)
+
+DELAY    LDY  #WAIT       ;Outer loop counter
+:OUTER   LDX  #$FF        ;Inner loop counter
+:INNER   DEX              ;Decrement inner
+         BNE  :INNER      ;Loop until X=0
+         DEY              ;Decrement outer
+         BNE  :OUTER      ;Loop until Y=0
+         RTS
 
 **********************************
 * Message data
