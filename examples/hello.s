181fd31b0b9388e7577086c34465dd657b7f3bb5
michaeldaley <michael_daley@icloud.com>
2026-02-05 10:55:37 +0000

    Fix hello.s - use ASC properly with separate DFB for terminator

--- examples/hello.s
+++ examples/hello.s
@@ -30,23 +30,9 @@ LOOP     LDA  MESSAGE,X   ;Load character at MESSAGE+X
 DONE     RTS              ;Return to caller
 
 **********************************
-* Message data - high bit set for Apple II
+* Message data
+* Using " delimiter sets high bit automatically
 **********************************
-MESSAGE  DFB  $C8         ;H
-         DFB  $C5         ;E
-         DFB  $CC         ;L
-         DFB  $CC         ;L
-         DFB  $CF         ;O
-         DFB  $A0         ;(space)
-         DFB  $C1         ;A
-         DFB  $D0         ;P
-         DFB  $D0         ;P
-         DFB  $CC         ;L
-         DFB  $C5         ;E
-         DFB  $A0         ;(space)
-         DFB  $C9         ;I
-         DFB  $C9         ;I
-         DFB  $C5         ;E
-         DFB  $A1         ;!
-         DFB  $8D         ;Carriage return
+MESSAGE  ASC  "HELLO APPLE IIE!"
+         DFB  $8D         ;Carriage return (with high bit)
          DFB  $00         ;Null terminator
