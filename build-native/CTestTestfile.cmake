ac3e90e2a871de02932b213d094934fcada73443
Mike Daley <michael_daley@icloud.com>
2026-02-12 11:44:58 +0000

    Latest

--- build-native/CTestTestfile.cmake
+++ build-native/CTestTestfile.cmake
@@ -5,10 +5,10 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[klaus_65c02_extended_test]=] "/Users/michaeldaley/Source/web-a2e/build-native/klaus_65c02_test")
-set_tests_properties([=[klaus_65c02_extended_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;272;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
+set_tests_properties([=[klaus_65c02_extended_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;335;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
 add_test([=[klaus_6502_functional_test]=] "/Users/michaeldaley/Source/web-a2e/build-native/klaus_6502_test")
-set_tests_properties([=[klaus_6502_functional_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;287;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
+set_tests_properties([=[klaus_6502_functional_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;350;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
 add_test([=[thunderclock_card_test]=] "/Users/michaeldaley/Source/web-a2e/build-native/thunderclock_test")
-set_tests_properties([=[thunderclock_card_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;310;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
+set_tests_properties([=[thunderclock_card_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;373;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
 add_test([=[thunderclock_mmu_integration_test]=] "/Users/michaeldaley/Source/web-a2e/build-native/thunderclock_mmu_test")
-set_tests_properties([=[thunderclock_mmu_integration_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;328;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
+set_tests_properties([=[thunderclock_mmu_integration_test]=] PROPERTIES  WORKING_DIRECTORY "/Users/michaeldaley/Source/web-a2e" _BACKTRACE_TRIPLES "/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;391;add_test;/Users/michaeldaley/Source/web-a2e/CMakeLists.txt;0;")
